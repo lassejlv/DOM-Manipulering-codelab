13f93260756be0cb98cf41e1fb75ff26a18cf8b7
lassev05 <vestergaardlasse2@gmail.com>
2023-11-20 09:15:17 +0100

    Update domelementer.js

--- assets/js/opgaver/domelementer.js
+++ assets/js/opgaver/domelementer.js
@@ -1,77 +1,82 @@
-/* DOMcelementer */
-
-
+/* DOM-elementer */
 
 /* Opgave 1.1
 Du skal finde elementet med id="redFigure" og console logge det.
-Du skal finde elementet med id="yellowFigure" og console logge det. 
+Du skal finde elementet med id="yellowFigure" og console logge det.
 */
 
-// din kode her
+const redFigure = document.getElementById("redFigure");
+const yellowFigure = document.getElementById("yellowFigure");
 
+console.log(redFigure);
+console.log(yellowFigure);
 
-/* opgave 1.2
-Du skal ændre baggrundsfarve på de to elementer du har fundet i opgave 1.1
-farven på elementerne skal være blå. brug evt. elelement.style.backgroundColor
+/* Opgave 1.2
+Du skal ændre baggrundsfarve på de to elementer, du har fundet i opgave 1.1
+Farven på elementerne skal være blå. Brug evt. elelement.style.backgroundColor
 */
 
+redFigure.style.backgroundColor = "blue";
+yellowFigure.style.backgroundColor = "blue";
 
-// din kode her
-
-
-
-/* opgave 2.1
-Du skal oprette følgende HTML elementer i Elementet med id opgaveTwo.
+/* Opgave 2.1
+Du skal oprette følgende HTML-elementer i elementet med id opgaveTwo.
 - et h2 med teksten: Opgave 2.1 løsning
 - et p med teksten: Jeg har løst opgave 2.1
 */
 
+const opgaveTwoContainer = document.getElementById("opgaveTwo");
 
-// din kode her
+const h2Element = document.createElement("h2");
+h2Element.textContent = "Opgave 2.1 løsning";
 
+const pElement = document.createElement("p");
+pElement.textContent = "Jeg har løst opgave 2.1";
 
-// opgave 3 liveHTML lists
-/* opgave 3.1
-Du skal finde alle elementer med klassen purpleFigures og console logge resultatet
+opgaveTwoContainer.appendChild(h2Element);
+opgaveTwoContainer.appendChild(pElement);
+
+/* Opgave 3 liveHTML lists
+   Opgave 3.1
+   Du skal finde alle elementer med klassen purpleFigures og console logge resultatet
 */
 
-// din kode her
+const purpleFigures = document.getElementsByClassName("purpleFigures");
+console.log(purpleFigures);
 
-
-/* opgave 3.2
-Du skal finde alle elementer med klassen purpleFigures og bruge array.from() til at konvertere listen
-til et array hvor du kan bruge map funktionen til at ændre baggrundsfarven til rød.
-array.from beskrivelse HER: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/from
+/* Opgave 3.2
+   Du skal finde alle elementer med klassen purpleFigures og bruge array.from() til at konvertere listen
+   til et array, hvor du kan bruge map-funktionen til at ændre baggrundsfarven til rød.
 */
 
-// din kode her
+const purpleFiguresArray = Array.from(purpleFigures);
+purpleFiguresArray.map((element) => (element.style.backgroundColor = "red"));
 
-
-/* opgave 3.3
-Brug din array til at ændre h3 elementets, inden i purpleFigures, tekst til RED. du kan bruge
-myElement.children[0].innerHTML til at ændre 1. child elements inner html.
+/* Opgave 3.3
+   Brug din array til at ændre h3 elementets, inden i purpleFigures, tekst til RED.
+   Du kan bruge myElement.children[0].innerHTML til at ændre 1. child elements innerHTML.
 */
 
+purpleFiguresArray.map((element) => (element.children[0].innerHTML = "RED"));
 
-// din kode her
-
-
-
-/* opgave 4
-opret DOM elementer i elementet med id opgaveFour, udfra data objectet myData.
-Du skal vælge element tags der giver mening i forbindelse med HTML5 semantisk tagging.
-Fyld dine elementers innerHtML eller andre atributter med dine data
+/* Opgave 4
+   Opret DOM-elementer i elementet med id opgaveFour, ud fra dataobjektet myData.
+   Du skal vælge element tags, der giver mening i forbindelse med HTML5 semantisk tagging.
+   Fyld dine elementers innerHTML eller andre attributter med dine data.
 */
 
-const myData = {
-    name: 'tiger',
-    image: 'assets/img/standard_tiger.jpg',
-    description: 'En tiger er et stort, katteagtig rovdyr med orange og sort striber i pelsen og skarpe tænder og kløer.'
-};
+const opgaveFourContainer = document.getElementById("opgaveFour");
 
+const nameElement = document.createElement("h2");
+nameElement.textContent = myData.name;
 
-// din kode her
+const imageElement = document.createElement("img");
+imageElement.src = myData.image;
+imageElement.alt = myData.name;
 
+const descriptionElement = document.createElement("p");
+descriptionElement.textContent = myData.description;
 
-
-
+opgaveFourContainer.appendChild(nameElement);
+opgaveFourContainer.appendChild(imageElement);
+opgaveFourContainer.appendChild(descriptionElement);
